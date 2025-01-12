20007b6018defe1eda2e373cfb90ef236a0b14b9
utkina-ekaterina <utkinaek@my.yorku.ca>
2025-01-12 00:20:45 -0500

    added buttons, added functionality

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceImages = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+        ];
+        
+        let leftDiceNumber = Int.random(in: 0...5);
+        let rightDiceNumber = Int.random(in: 0...5);
+        
+        diceImageView1.image = diceImages[leftDiceNumber];
+        diceImageView2.image = diceImages[rightDiceNumber];
+        
+    }
+    
+}
+
